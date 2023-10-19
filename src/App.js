184c9684775990517e3db5105c70005f6f1342bf
saneep-pp <saneepprince@gmail.com>
2023-10-19 18:35:22 +0530

    Design Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Color Changer</h1>
       </header>
+      <h2 className='subHead'>Select the Color</h2>
+      <hr/>
+      <div className='main'>
+        <div className='colors'>
+        <div className='color red'>Red</div>
+        <div className='color green'>Green</div>
+        <div className='color blue'>Blue</div>
+        <div className='color violet'>Violet</div>
+        <div className='color grey'>Grey</div>
+        <div className='color yellow'>Yellow</div>
+        <div className='color orange'>Orange</div>
+        <div className='color purple'>Purple</div>
+        <div className='color magenta'>Magenta</div>
+        <div className='color palegreen'>Pale Green</div>
+        <div className='color maroon'>Maroon</div>
+        <div className='color skyblue'>Skyblue</div>
+        <div className='color brown'>Brown</div>
+        <div className='color silver'>Silver</div>
+        <div className='color gold'>Gold</div>
+        <div className='color aqua'>Aqua</div>
+        <div className='color pink'>Pink</div>
+        <div className='color yellowGreen'>Yellow Green</div>
+        </div>
+      </div>
     </div>
   );
 }
